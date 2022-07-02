8d4a8646340a430d9eea3cde5c16b2208ab9f56e
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-07-02 19:18:09 +0200

    feat: init

--- src/mock/data.js
+++ src/mock/data.js
@@ -28,30 +28,56 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
+    img: 'meetup.png',
+    title: 'Meetup',
     info: '',
     info2: '',
-    url: '',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
+    img: 'notes.png',
+    title: 'Notes',
     info: '',
     info2: '',
-    url: '',
+    url: 'https://react-strapi-notes-app.netlify.app/',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
+    img: 'rick_and_morty.png',
+    title: 'Rick and Morty Website in Gatsby',
     info: '',
     info2: '',
-    url: '',
+    url: 'https://gatsby-rick-and-morty-graphql.netlify.app/',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'rick_and_morty.png',
+    title: 'Rick and Morty Website in React',
+    info: '',
+    info2: '',
+    url: 'https://react-rick-and-morty-graphql.netlify.app/',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'building_designing.png',
+    title: 'Building Designing Landing Page',
+    info: '',
+    info2: '',
+    url: 'https://gatsby-building-designing-landing-page.netlify.app/',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'mastermind.png',
+    title: 'Mastermind',
+    info: '',
+    info2: '',
+    url: 'https://mastermind-javascript.netlify.app/',
+    repo: 'https://github.com/JakubZakowicz/javascript-simple-games/tree/main/MasterMind', // if no repo, the button will not show up
   },
 ];
 
