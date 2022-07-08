be36d483dfd87210ec93e786ab4d2fd2e7552e90
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-07-08 21:20:31 +0200

    add animation to skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,86 +1,104 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
+import Fade from 'react-reveal/Fade';
 import Title from '../Title/Title';
 
 const Skills = () => {
   return (
     <section id="skills">
       <Title title="Skills" />
-      <div className="skills-wrapper">
-        <div className="skills-wrapper__skill">
-          <div className="skills-wrapper__image">
-            <StaticImage alt="skill picture" src="../../images/html.png" width="200" height="200" />
+      <Fade bottom duration={1000} delay={800} distance="100px">
+        <div className="skills-wrapper">
+          <div className="skills-wrapper__skill">
+            <div className="skills-wrapper__image">
+              <StaticImage
+                alt="skill picture"
+                src="../../images/html.png"
+                width="200"
+                height="200"
+              />
+            </div>
+            <p className="skills-wrapper__text">HTML</p>
           </div>
-          <p className="skills-wrapper__text">HTML</p>
-        </div>
-        <div className="skills-wrapper__skill">
-          <div className="skills-wrapper__image">
-            <StaticImage alt="skill picture" src="../../images/css.png" width="200" height="200" />
+          <div className="skills-wrapper__skill">
+            <div className="skills-wrapper__image">
+              <StaticImage
+                alt="skill picture"
+                src="../../images/css.png"
+                width="200"
+                height="200"
+              />
+            </div>
+            <p className="skills-wrapper__text">CSS</p>
           </div>
-          <p className="skills-wrapper__text">CSS</p>
-        </div>
-        <div className="skills-wrapper__skill">
-          <div className="skills-wrapper__image">
-            <StaticImage
-              alt="skill picture"
-              src="../../images/javascript.png"
-              width="200"
-              height="200"
-            />
+          <div className="skills-wrapper__skill">
+            <div className="skills-wrapper__image">
+              <StaticImage
+                alt="skill picture"
+                src="../../images/javascript.png"
+                width="200"
+                height="200"
+              />
+            </div>
+            <p className="skills-wrapper__text">JavaScript</p>
           </div>
-          <p className="skills-wrapper__text">JavaScript</p>
-        </div>
-        <div className="skills-wrapper__skill">
-          <div className="skills-wrapper__image">
-            <StaticImage
-              alt="skill picture"
-              src="../../images/typescript.png"
-              width="200"
-              height="200"
-            />
+          <div className="skills-wrapper__skill">
+            <div className="skills-wrapper__image">
+              <StaticImage
+                alt="skill picture"
+                src="../../images/typescript.png"
+                width="200"
+                height="200"
+              />
+            </div>
+            <p className="skills-wrapper__text">TypeScript</p>
           </div>
-          <p className="skills-wrapper__text">TypeScript</p>
-        </div>
-        <div className="skills-wrapper__skill">
-          <div className="skills-wrapper__image">
-            <StaticImage
-              alt="skill picture"
-              src="../../images/react.png"
-              width="200"
-              height="200"
-            />
+          <div className="skills-wrapper__skill">
+            <div className="skills-wrapper__image">
+              <StaticImage
+                alt="skill picture"
+                src="../../images/react.png"
+                width="200"
+                height="200"
+              />
+            </div>
+            <p className="skills-wrapper__text">React</p>
           </div>
-          <p className="skills-wrapper__text">React</p>
-        </div>
-        <div className="skills-wrapper__skill">
-          <div className="skills-wrapper__image">
-            <StaticImage
-              alt="skill picture"
-              src="../../images/redux.png"
-              width="220"
-              height="200"
-            />
+          <div className="skills-wrapper__skill">
+            <div className="skills-wrapper__image">
+              <StaticImage
+                alt="skill picture"
+                src="../../images/redux.png"
+                width="220"
+                height="200"
+              />
+            </div>
+            <p className="skills-wrapper__text">Redux</p>
           </div>
-          <p className="skills-wrapper__text">Redux</p>
-        </div>
-        <div className="skills-wrapper__skill">
-          <div className="skills-wrapper__image">
-            <StaticImage
-              alt="skill picture"
-              src="../../images/gatsby.png"
-              width="200"
-              height="200"
-            />
+          <div className="skills-wrapper__skill">
+            <div className="skills-wrapper__image">
+              <StaticImage
+                alt="skill picture"
+                src="../../images/gatsby.png"
+                width="200"
+                height="200"
+              />
+            </div>
+            <p className="skills-wrapper__text">Gatsby</p>
           </div>
-          <p className="skills-wrapper__text">Gatsby</p>
-        </div>
-        <div className="skills-wrapper__skill">
-          <div className="skills-wrapper__image">
-            <StaticImage alt="skill picture" src="../../images/git.png" width="200" height="200" />
+          <div className="skills-wrapper__skill">
+            <div className="skills-wrapper__image">
+              <StaticImage
+                alt="skill picture"
+                src="../../images/git.png"
+                width="200"
+                height="200"
+              />
+            </div>
+            <p className="skills-wrapper__text">Git</p>
           </div>
-          <p className="skills-wrapper__text">Git</p>
         </div>
-      </div>
+      </Fade>
     </section>
   );
 };
